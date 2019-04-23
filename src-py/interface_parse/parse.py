8855885f81f11c4e22d72f767a758dd1e2ada501
ducvd <ducvd@aimesoft.com>
2019-04-23 14:43:19 +0700

    Src PYclass-

---
+++ src-py/interface_parse/parse.py
@@ -0,0 +1,12 @@
+from os import walk
+
+#
+# TODO: 
+#    Enum all files with-in directory
+#    Listing all class/struct/
+# 
+
+f = []
+
+for (dirpath, dirnames, filenames) in walk('~/ducvd'):
+    
