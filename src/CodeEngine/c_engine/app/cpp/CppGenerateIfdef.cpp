295d6003a93071c109d8013a6d62a1d7026e591d
Duy Duc Van <dvan@tibco.com>
2021-01-20 10:03:21 +0700

    work

---
+++ src/CodeEngine/c_engine/app/cpp/CppGenerateIfdef.cpp
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <conio.h>
+#include <iostream>
+#include <string>
+#include "c_engine/impl/toolkit/CStringToolkit.h"
+#include "c_engine/interface/toolkit/Console.h"
+#include "c_engine/impl/engine/cpp/CCppGeneral.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    CCppGeneral cpp;
+    printf("App01\r\n");
+    return 0;
+}
